0e52e10296cb47e268f9e378ae0637dd7ec26d59
zpilat <zdenek.pilat@seznam.cz>
2024-07-09 21:05:25 +0200

    funkce loaddetail a loadform vytaženy pro správnou funkci vně DOMContentLoaded listeneru

--- hpm_sklad/static/js/scripts.js
+++ hpm_sklad/static/js/scripts.js
@@ -1,3 +1,19 @@
+// Funkce pro načtení detailu a zvýraznění řádku
+function loadDetail(url, evidencniCislo) {
+    fetch(url)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('detail').innerHTML = html;
+        // Aktualizace URL a zvýraznění řádku
+        history.pushState(null, '', '?selected=' + evidencniCislo); // Aktualizace URL
+        document.querySelectorAll('tr').forEach(row => {
+            row.classList.remove('table-info');
+        });
+        document.querySelector('tr[data-id="' + evidencniCislo + '"]').classList.add('table-info');
+    })
+    .catch(err => console.error('Chyba při načítání detailů:', err));
+}
+
 // Funkce pro načtení formulářů (vytvořit/upravit/smazat) do pravého sloupce
 function loadForm(url) {
     fetch(url)
@@ -6,57 +22,30 @@ function loadForm(url) {
         document.getElementById('detail').innerHTML = html;
     })
     .catch(err => console.error('Chyba při načítání formuláře:', err));
-}    
-
-
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    function loadDetail(url, id) {
-        fetch(url)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('detail').innerHTML = html;
-            // Aktualizace URL a zvýraznění řádku
-            history.pushState(null, '', '?selected=' + id);
-            document.querySelectorAll('tr').forEach(row => {
-                row.classList.remove('table-info');
-            });
-            document.querySelector('tr[data-id="' + id + '"]').classList.add('table-info');
-        })
-        .catch(err => console.error('Chyba při načítání detailů:', err));
-    }
-    
-    // Funkce pro načtení formulářů (vytvořit/upravit/smazat) do pravého sloupce
-    function loadForm(url) {
-        fetch(url)
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('detail').innerHTML = html;
-        })
-        .catch(err => console.error('Chyba při načítání formuláře:', err));
-    }    
-
     function selectFirstRow() {
-        const firstRow = document.querySelector('table tbody tr:first-child'); // První řádek v těle tabulky
+        const firstRow = document.querySelector('table tbody tr:nth-child(1)'); // První řádek v těle tabulky
         if (firstRow) {
-            const id = firstRow.getAttribute('data-id');
+            const evidencniCislo = firstRow.getAttribute('data-id');
             const detailUrl = firstRow.getAttribute('data-detail-url');
-            loadDetail(detailUrl, id);
+            loadDetail(detailUrl, evidencniCislo);
         }
     }
 
     // Automaticky vyberte první řádek při načtení stránky
-    selectFirstRow();
+    selectFirstRow();    
 
     // Přidání onclick události ke všem řádkům
     document.querySelectorAll('table tbody tr').forEach(row => {
         row.addEventListener('click', function() {
-            const id = this.getAttribute('data-id');
+            const evidencniCislo = this.getAttribute('data-id');
             const detailUrl = this.getAttribute('data-detail-url');
-            loadDetail(detailUrl, id);
+            loadDetail(detailUrl, evidencniCislo);
         });
     });
-    
+
     // Přidání onclick události k odkazům, které načítají formuláře
     document.querySelectorAll('a[data-load-form]').forEach(link => {
         link.addEventListener('click', function(event) {
